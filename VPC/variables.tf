6de40177b805e027843ae41741bcb045fce04b07
freddiet1 <86828760+freddiet1@users.noreply.github.com>
2021-08-24 01:10:04 -0500

    Update variables.tf

--- VPC/variables.tf
+++ VPC/variables.tf
@@ -3,7 +3,7 @@
  with your key name which  you have in given aws_region.
 */
 variable "aws_region" {
-  default = "us-east-2"
+  default = "us-east-1"
 }
 variable "vpc_cidr" {
   default = "172.0.0.0/24"
